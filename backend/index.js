d6dceb6e0a13a73b12052c881f38a22949c5b53c
Sajjad <muhammadsajjad1798@gmail.com>
2021-07-09 03:55:13 -0700

    clean code

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,28 @@
 require("dotenv").config();
 const express = require("express")
+
+// custom files 
+
 const connection = require("./config/db");
 const router = require("./routes/userRoutes")
+
 const app = express();
 
-//connection to database
+// Connection to database
 
-connection();
+connection(); 
 
-app.use(express.json()) //For JSON requests
+// Midddle Ware 
+app.use(express.json()) 
 app.use(express.urlencoded({extended: true}));
 app.use("/" , router)
+
+// Port Number where my app run
+
 const PORT = process.env.PORT || 5000;
+
+// app listen on the port 
+
 app.listen(PORT , () => {
  console.log(`App is listing on the port number ${PORT}`);
 })
